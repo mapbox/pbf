cb9f5091e69b0574eb42f64e8736307f745a8c58
Ingvar Stepanyan <me@rreverser.com>
2016-05-26 16:28:30 +0100

    Regenerate bench/vector_tile.js

--- bench/vector_tile.js
+++ bench/vector_tile.js
@@ -36,20 +36,20 @@ function readValueField(tag, value, pbf) {
     if (tag === 1) value.string_value = pbf.readString();
     else if (tag === 2) value.float_value = pbf.readFloat();
     else if (tag === 3) value.double_value = pbf.readDouble();
-    else if (tag === 4) value.int_value = pbf.readVarint64();
+    else if (tag === 4) value.int_value = pbf.readVarint();
     else if (tag === 5) value.uint_value = pbf.readVarint();
     else if (tag === 6) value.sint_value = pbf.readSVarint();
     else if (tag === 7) value.bool_value = pbf.readBoolean();
 }
 
 function writeValue(value, pbf) {
-    if (value.string_value !== undefined) pbf.writeString(1, value.string_value);
-    if (value.float_value !== undefined) pbf.writeFloat(2, value.float_value);
-    if (value.double_value !== undefined) pbf.writeDouble(3, value.double_value);
-    if (value.int_value !== undefined) pbf.writeVarint(4, value.int_value);
-    if (value.uint_value !== undefined) pbf.writeVarint(5, value.uint_value);
-    if (value.sint_value !== undefined) pbf.writeSVarint(6, value.sint_value);
-    if (value.bool_value !== undefined) pbf.writeBoolean(7, value.bool_value);
+    if (value.string_value !== undefined) pbf.writeStringField(1, value.string_value);
+    if (value.float_value !== undefined) pbf.writeFloatField(2, value.float_value);
+    if (value.double_value !== undefined) pbf.writeDoubleField(3, value.double_value);
+    if (value.int_value !== undefined) pbf.writeVarintField(4, value.int_value);
+    if (value.uint_value !== undefined) pbf.writeVarintField(5, value.uint_value);
+    if (value.sint_value !== undefined) pbf.writeSVarintField(6, value.sint_value);
+    if (value.bool_value !== undefined) pbf.writeBooleanField(7, value.bool_value);
 }
 
 // Feature ========================================
@@ -70,9 +70,9 @@ function readFeatureField(tag, feature, pbf) {
 }
 
 function writeFeature(feature, pbf) {
-    if (feature.id !== undefined) pbf.writeVarint(1, feature.id);
+    if (feature.id !== undefined) pbf.writeVarintField(1, feature.id);
     if (feature.tags !== undefined) pbf.writePackedVarint(2, feature.tags);
-    if (feature.type !== undefined) pbf.writeVarint(3, feature.type);
+    if (feature.type !== undefined) pbf.writeVarintField(3, feature.type);
     if (feature.geometry !== undefined) pbf.writePackedVarint(4, feature.geometry);
 }
 
@@ -94,11 +94,11 @@ function readLayerField(tag, layer, pbf) {
 }
 
 function writeLayer(layer, pbf) {
-    if (layer.version !== undefined) pbf.writeVarint(15, layer.version);
-    if (layer.name !== undefined) pbf.writeString(1, layer.name);
+    if (layer.version !== undefined) pbf.writeVarintField(15, layer.version);
+    if (layer.name !== undefined) pbf.writeStringField(1, layer.name);
     var i;
     if (layer.features !== undefined) for (i = 0; i < layer.features.length; i++) pbf.writeMessage(2, writeFeature, layer.features[i]);
-    if (layer.keys !== undefined) for (i = 0; i < layer.keys.length; i++) pbf.writeString(3, layer.keys[i]);
+    if (layer.keys !== undefined) for (i = 0; i < layer.keys.length; i++) pbf.writeStringField(3, layer.keys[i]);
     if (layer.values !== undefined) for (i = 0; i < layer.values.length; i++) pbf.writeMessage(4, writeValue, layer.values[i]);
-    if (layer.extent !== undefined) pbf.writeVarint(5, layer.extent);
+    if (layer.extent !== undefined) pbf.writeVarintField(5, layer.extent);
 }
