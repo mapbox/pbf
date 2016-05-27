66092cb2a6fca893c81ce08485175f3629c2e0e1
Vladimir Agafonkin <agafonkin@gmail.com>
2016-05-28 00:09:38 +0300

    add a compile test

---
+++ test/compile.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var test = require('tap').test;
+var resolve = require('resolve-protobuf-schema').sync;
+
+var Pbf = require('../');
+var compile = require('../compile');
+
+test('compiles vector tile proto', function(t) {
+    var proto = resolve(path.join(__dirname, '../bench/vector_tile.proto'));
+    var tileBuf = fs.readFileSync(path.join(__dirname, 'fixtures/12665.vector.pbf'));
+    var Tile = compile(proto).Tile;
+
+    var tile = Tile.read(new Pbf(tileBuf));
+    t.equal(tile.layers.length, 11);
+
+    var pbf = new Pbf();
+    Tile.write(tile, pbf);
+    var buf = pbf.finish();
+    t.equal(buf.length, 125104);
+
+    t.end();
+});
