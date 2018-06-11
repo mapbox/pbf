fa1b00275c91c31a0819ba59aa13077f7144f9bd
Ashley Rose <ash@ley-ro.se>
2018-06-11 16:27:26 -0400

    Add protobuf.js to benchmarks

--- bench/bench.js
+++ bench/bench.js
@@ -2,44 +2,53 @@
 
 var Benchmark = require('benchmark'),
     fs = require('fs'),
-    protobuf = require('protocol-buffers'),
+    path = require('path'),
+    protocolBuffers = require('protocol-buffers'),
+    protobufjs = require('protobufjs'),
     vt = require('./vector_tile'),
-    Pbf = require('../'),
-    readTile = vt.Tile.read,
-    writeTile = vt.Tile.write;
+    Pbf = require('../');
 
-var Tile = protobuf(fs.readFileSync(__dirname + '/vector_tile.proto')).Tile,
-    data = fs.readFileSync(__dirname + '/../test/fixtures/12665.vector.pbf'),
-    suite = new Benchmark.Suite();
+var pbfReadTile = vt.Tile.read,
+    pbfWriteTile = vt.Tile.write,
+    data = fs.readFileSync(path.resolve(__dirname, '../test/fixtures/12665.vector.pbf')),
+    suite = new Benchmark.Suite(),
+    ProtocolBuffersTile = protocolBuffers(fs.readFileSync(path.resolve(__dirname, 'vector_tile.proto'))).Tile,
+    ProtobufjsTile = protobufjs.loadSync(path.resolve(__dirname, 'vector_tile.proto'))
+        .lookup('vector_tile.Tile');
 
-var tile = readTile(new Pbf(data)),
-    tileJSON = JSON.stringify(tile),
-    tile2 = Tile.decode(data);
-
-writeTile(tile, new Pbf());
+var pbfTile = pbfReadTile(new Pbf(data)),
+    tileJSON = JSON.stringify(pbfTile),
+    protocolBuffersTile = ProtocolBuffersTile.decode(data),
+    protobufjsTile = ProtobufjsTile.decode(data);
 
 suite
-.add('decode vector tile with pbf', function() {
-    readTile(new Pbf(data));
-})
-.add('encode vector tile with pbf', function() {
-    var pbf = new Pbf();
-    writeTile(tile, pbf);
-    pbf.finish();
-})
-.add('decode vector tile with protocol-buffers', function() {
-    Tile.decode(data);
-})
-.add('encode vector tile with protocol-buffers', function() {
-    Tile.encode(tile2);
-})
-.add('JSON.parse vector tile', function() {
-    JSON.parse(tileJSON);
-})
-.add('JSON.stringify vector tile', function() {
-    JSON.stringify(tile);
-})
-.on('cycle', function(event) {
-    console.log(String(event.target));
-})
-.run();
+    .add('decode vector tile with pbf', function() {
+        pbfReadTile(new Pbf(data));
+    })
+    .add('encode vector tile with pbf', function() {
+        var pbf = new Pbf();
+        pbfWriteTile(pbfTile, pbf);
+        pbf.finish();
+    })
+    .add('decode vector tile with protocol-buffers', function() {
+        ProtocolBuffersTile.decode(data);
+    })
+    .add('encode vector tile with protocol-buffers', function() {
+        ProtocolBuffersTile.encode(protocolBuffersTile);
+    })
+    .add('decode vector tile with protobuf.js', function() {
+        ProtobufjsTile.decode(data);
+    })
+    .add('encode vector tile with protobuf.js', function() {
+        ProtobufjsTile.encode(protobufjsTile);
+    })
+    .add('JSON.parse vector tile', function() {
+        JSON.parse(tileJSON);
+    })
+    .add('JSON.stringify vector tile', function() {
+        JSON.stringify(pbfTile);
+    })
+    .on('cycle', function(event) {
+        console.log(String(event.target));
+    })
+    .run();
